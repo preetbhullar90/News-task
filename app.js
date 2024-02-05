72568b503808a9e284d33cc27577f9e27d2f7dd1
gurpreetbhullar70 <gurpreet.singhb90@gmail.com>
2024-02-05 09:25:51 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const {getAllTopics} = require("./controllers/news.controllers");
 const {availableEndpoints} = require("./controllers/endpoints.controller");
 const {
